7271bd6d9ce44dda5b9883d8f50e8de7aa484201
lrdcxdes <lordgrief176@gmail.com>
2024-10-29 16:48:54 +0200

    bump database

--- src/main/kotlin/dev/lrdcxdes/hardcraft/sql/Database.kt
+++ src/main/kotlin/dev/lrdcxdes/hardcraft/sql/Database.kt
@@ -1,9 +1,11 @@
 package dev.lrdcxdes.hardcraft.sql
 
 import dev.lrdcxdes.hardcraft.Hardcraft
+import org.bukkit.Material
 import java.sql.Connection
 import java.sql.DriverManager
 import java.io.File
+import java.sql.ResultSet
 
 class DatabaseManager {
     private val plugin = Hardcraft.instance
@@ -35,7 +37,7 @@ class DatabaseManager {
             this.connection.prepareStatement(
                 """
                     CREATE TABLE IF NOT EXISTS project9_players (
-                        id INT PRIMARY KEY,
+                        id INTEGER PRIMARY KEY AUTOINCREMENT,
                         uuid TEXT NOT NULL,
                         username VARCHAR(16) NOT NULL,
                         balance DOUBLE NOT NULL,
@@ -62,6 +64,37 @@ class DatabaseManager {
             ).use { ps ->
                 ps.execute()
                 plugin.logger.info("Created table 'project9_friends'!")
+            }
+
+            connection.prepareStatement(
+                """
+                CREATE TABLE IF NOT EXISTS project9_items (
+                    id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    material VARCHAR(48) NOT NULL,
+                    init_price DOUBLE NOT NULL,
+                    init_volume DOUBLE NOT NULL,
+                    volume DOUBLE NOT NULL
+                )
+                """.trimIndent()
+            ).use { ps ->
+                ps.execute()
+                plugin.logger.info("Created table 'project9_items'!")
+            }
+
+            // config table
+            connection.prepareStatement(
+                """
+                CREATE TABLE IF NOT EXISTS project9_config (
+                    id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    k VARCHAR(256) NOT NULL,
+                    v VARCHAR(256) NOT NULL,
+                    t VARCHAR(16) NOT NULL,
+                    UNIQUE (k)
+                )
+                """.trimIndent()
+            ).use { ps ->
+                ps.execute()
+                plugin.logger.info("Created table 'project9_config'!")
             }
         } catch (e: Exception) {
             plugin.logger.severe("Error creating tables: ${e.message}")
@@ -281,7 +314,7 @@ class DatabaseManager {
         return 0.0
     }
 
-    fun has(uuid: String, amount: Double): Boolean {
+    fun havePlayer(uuid: String, amount: Double): Boolean {
         try {
             connection.prepareStatement(
                 """
@@ -300,5 +333,178 @@ class DatabaseManager {
             plugin.logger.severe("Error getting player uuid: ${e.message}")
         }
         return false
+    }
+
+    fun hasItem(material: Material): Boolean {
+        // Check if item exists
+        try {
+            connection.prepareStatement(
+                """
+                    SELECT COUNT(*)
+                    FROM project9_items
+                    WHERE material = ?
+                    """.trimIndent()
+            ).use { sql ->
+                sql.setString(1, material.name)
+                val result = sql.executeQuery()
+                if (result.next()) {
+                    return result.getInt(1) > 0
+                }
+            }
+        } catch (e: Exception) {
+            plugin.logger.severe("Error checking if item exists: ${e.message}")
+        }
+
+        return false
+    }
+
+    fun addItem(material: Material, initPrice: Double, volume: Double) {
+        // Add item to database
+        try {
+            connection.prepareStatement(
+                """
+                    INSERT INTO project9_items (material, init_price, init_volume, volume)
+                    VALUES (?, ?, ?, ?)
+                    """.trimIndent()
+            ).use { sql ->
+                sql.setString(1, material.name)
+                sql.setDouble(2, initPrice)
+                sql.setDouble(3, volume)
+                sql.setDouble(4, volume)
+                sql.execute()
+            }
+        } catch (e: Exception) {
+            plugin.logger.severe("Error adding item: ${e.message}")
+        }
+    }
+
+    fun getItem(material: Material): ResultSet? {
+        // Get item from database
+        try {
+            val sql = connection.prepareStatement(
+                """
+                    SELECT *
+                    FROM project9_items
+                    WHERE material = ?
+                    """.trimIndent()
+            )
+            sql.setString(1, material.name)
+            val result = sql.executeQuery()
+            if (result.next()) {
+                return result
+            }
+        } catch (e: Exception) {
+            plugin.logger.severe("Error getting item: ${e.message}")
+        }
+
+        return null
+    }
+
+    fun updateItem(id: Int, initPrice: Double, initVolume: Double) {
+        // Update item in database
+        try {
+            connection.prepareStatement(
+                """
+                    UPDATE project9_items
+                    SET init_price = ?, init_volume = ?
+                    WHERE id = ?
+                    """.trimIndent()
+            ).use { sql ->
+                sql.setDouble(1, initPrice)
+                sql.setDouble(2, initVolume)
+                sql.setInt(3, id)
+                sql.execute()
+            }
+        } catch (e: Exception) {
+            plugin.logger.severe("Error updating item: ${e.message}")
+        }
+    }
+
+    fun changeItemVolume(id: Int, amount: Double, action: String) {
+        // Change item's volume
+        try {
+            connection.prepareStatement(
+                """
+                    UPDATE project9_items
+                    SET volume = CASE
+                        WHEN ? = 'set' THEN ?
+                        WHEN ? = 'add' THEN volume + ?
+                        WHEN ? = 'remove' THEN volume - ?
+                    END
+                    WHERE id = ?
+                    """.trimIndent()
+            ).use { sql ->
+                sql.setString(1, action)
+                sql.setDouble(2, amount)
+                sql.setString(3, action)
+                sql.setDouble(4, amount)
+                sql.setString(5, action)
+                sql.setDouble(6, amount)
+                sql.setInt(7, id)
+                sql.execute()
+            }
+        } catch (e: Exception) {
+            plugin.logger.severe("Error changing item volume: ${e.message}")
+        }
+    }
+
+    private val typeMaps = mapOf<String, (ResultSet) -> Any>(
+        "int" to { rs -> rs.getInt("v") },
+        "double" to { rs -> rs.getDouble("v") },
+        "string" to { rs -> rs.getString("v") },
+        "long" to { rs -> rs.getLong("v") }
+    )
+
+    fun getConfig(key: String): Any? {
+        // Get config value
+        try {
+            connection.prepareStatement(
+                """
+                    SELECT v, t
+                    FROM project9_config
+                    WHERE k = ?
+                    """.trimIndent()
+            ).use { sql ->
+                sql.setString(1, key)
+                val result = sql.executeQuery()
+                if (result.next()) {
+                    return typeMaps[result.getString("t")]?.invoke(result)
+                }
+            }
+        } catch (e: Exception) {
+            plugin.logger.severe("Error getting config value: ${e.message}")
+        }
+
+        return null
+    }
+
+    private fun getType(value: Any): String {
+        return when (value) {
+            is Int -> "int"
+            is Double -> "double"
+            is String -> "string"
+            is Long -> "long"
+            else -> throw IllegalArgumentException("Unsupported type")
+        }
+    }
+
+    fun setConfig(key: String, value: Any) {
+        // Set config value
+        try {
+            connection.prepareStatement(
+                """
+                REPLACE INTO project9_config (k, v, t)
+                VALUES (?, ?, ?)
+                """.trimIndent()
+            ).use { sql ->
+                sql.setString(1, key)
+                sql.setObject(2, value)
+                val type = getType(value)
+                sql.setString(3, type)
+                sql.execute()
+            }
+        } catch (e: Exception) {
+            plugin.logger.severe("Error setting config value: ${e.message}")
+        }
     }
 }
